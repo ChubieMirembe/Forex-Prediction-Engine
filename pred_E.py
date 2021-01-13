bbba70e643ff5a04fe3c3049b040298a3167537a
Chubie Mirembe <71077124+ChubieMirembe@users.noreply.github.com>
2021-01-13 17:06:16 +0000

    Working segmentation

--- pred_E.py
+++ pred_E.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import plotly.express as px
+# import plotly.express as px
 
 # Importing the dataset and dropping unnecesary columns
 dataset = pd.read_csv('AUDUSD60.csv', delimiter='\t', names=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'], 
@@ -54,152 +54,94 @@ for g in range(0,len(group)):
     plt.show()
 
    
-
 #Segmentation Algorithm 
 pattern = []
 minArea = 5
-
 for i in range(0, len(group)):
-    miniPattern = []
+    checkA = 0
+    checkB = 0
+    halfA = []
+    halfB = []
     positiveC = 0
     negativeC = 0
     for di in range(0, len(group[i])):
-        if group[i]['Distance'][di] < 0:
-            miniPattern.append(group[i]['Distance'][di])
-            negativeC += 1
-            continue
-            
-        elif group[i]['Distance'][di] > 0:
-            miniPattern.append(group[i]['Distance'][di])
-            positiveC += 1
-            continue
-       
-        else:
-            if negativeC < minArea:
-                miniPattern.append(group[i]['Distance'][di])
-                negativeC = 0
-                positiveC += 1
-            
-            elif negativeC >= minArea:
-                miniPattern.append(group[i]['Distance'][di])
-                negativeC = 0
-                positiveC += 1
-                
-            elif positiveC < minArea:
-                miniPattern.append(group[i]['Distance'][di])
-                positiveC = 0
-                negativeC += 1
-            
-            elif positiveC >= minArea:
-                miniPattern.append(group[i]['Distance'][di])
-                positiveC = 0
-                negativeC += 1
-                
-    pattern.append(miniPattern)
-                
-
-pattern = []
-minArea = 5
-for i in range(0, len(group)):
-    zeroC = 0
-    count = 0
-    miniP = []
-    positiveC = 0
-    negativeC = 0
-    for di in range(0, len(group[i])):
-            if group[i]['Distance'][di] < 0:
-                if positiveC in range(1, 5):
-                    miniP.clear()
-                    positiveC = 0
+            if group[i]['Distance'][di] < 0:  
+                if checkA == 0:
+                    if len(halfB) < minArea:
+                        halfA.clear()
+                        halfB.clear()
+                        halfA.append(group[i].iloc[di])
+                        checkA += 1
+                        checkB = 0
+                    else:
+                        if len(halfA) < minArea:
+                            halfA.clear()
+                            # halfB.clear()
+                            halfA.append(group[i].iloc[di])
+                            checkA += 1
+                            checkB = 0
+                        else:
+                            full = halfA + halfB
+                            pattern.append(full)
+                            halfA.clear()
+                            halfB.clear()
+                            # full.clear()
+                            halfA.append(group[i].iloc[di])
+                            checkA += 1 
+                            checkB = 0
                 else:
-                    continue 
-                
-                miniP.append(group[i].iloc[di])
-                negativeC += 1
+                    halfA.append(group[i].iloc[di])
+                    checkB = 0
             
             elif group[i]['Distance'][di] > 0:
-                if negativeC in range(1,5):
-                    miniP.clear()
-                    negativeC = 0
+                if checkB == 0:
+                    if len(halfA) < minArea:
+                        halfA.clear()
+                        halfB.clear()
+                        halfB.append(group[i].iloc[di])
+                        checkB += 1
+                        checkA = 0
+                    else:
+                        if len(halfB) < minArea:
+                            # halfA.clear()
+                            halfB.clear()
+                            halfB.append(group[i].iloc[di])
+                            checkB += 1
+                            checkA = 0
+                        else:
+                            full = halfB + halfA
+                            pattern.append(full)
+                            halfA.clear()
+                            halfB.clear()
+                            # full.clear()
+                            halfB.append(group[i].iloc[di])
+                            checkB += 1
+                            checkA = 0
                 else:
-                    continue
+                    halfB.append(group[i].iloc[di])
+                    checkA = 0
                 
-                miniP.append(group[i].iloc[di])
-                positiveC += 1
 
-    pattern.append(miniP)
-            
-ps = pd.DataFrame(pattern[1], columns = ['Date', 'Close', 'Distance'])
+#Viewing the sgemented pattern 
+
+
+
+
+
+
+
+
+ps = pd.DataFrame(pattern[0], columns = ['Date', 'Close', 'Distance'])
 plt.bar(ps.index, ps['Distance'])
 plt.show()  
 
-'''
-zeroC = 0
-for v in range(0,len(group[10])):
-    continue
-'''   
-    
+full = halfA + halfB
 
-rs =(range(1,5))
-print(rs)
-plt.hist((group[12]['Distance'].values), bins = 15)
-plt.show
-    
-'''
-- resulant trend vector, is the coefficient(predicted price) and the distrancce at each point from real price
-    - will get nor marmalised
-- So start counting the index from the beggining, everytime a pattern is found store it, and start counting again f
-  from that index
-- The counter adds up, and is used to seperate into different trends based on the laerger number
-    
+ps = pd.DataFrame(pattern[0], columns = ['Date', 'Close', 'Distance'])
+plt.bar(ps.index, ps['Distance'])
+plt.show()  
 
-for loop to loop through every week
-loop for every point in that week
-start from the beginning, and go until price has tried to cross zero - check the area is above min 
-    - Shouldnt i check highest point?: use a line to identify this value/point
-
-draw the distribution line
-find the highs in each cycle: if its larger than 5(index) find the crossing point as exchange points
-worry about storing the first trend 
-loop for the rest
- 
-1. Second if and else is checking whether its an uptrend or downtrend
-    a. Will i need to store this info. before clustering?
-2. 
-
-
-
-I want it enter into the loop to check through the week
-checking for uptrends 
-if its an uptrend, then enter a condition
-    - carry on checking if its still up 
-        - append the values if it continues
-    - if its switches before it gets to 5 steps, then come out of the condition(could be while loop)
-    - if its more then start to store those values
-then starts check again for another down move
-
-
-
-
-rn its just looking through the pattern that start with more than 5 and collecting
-- if theyre not up to 5 the list will not got the rest, why its stopping at 3
-- if they're more than 5 it can carry on till the end
-- Empty list if the first move is up
-
-append thw first half, if lenght is longger than 5, then will start appending the second half
-- otherwise start the other side as the first half and repeat the process
-
-add zero cross
--to find one pattern in each week, will work on the rest later 
-
-repeat for the other side
-
-
-
-'''
-
-
-
+,
 
 
 
@@ -224,17 +166,3 @@ repeat for the other side
 
 #DTW                
 
-
-'''
-Segementation Algorithm!!
-    1. Don't need to seperate
-        a. The K-Means algorithm will split 
-            i. You might want to check if the the optimum number of clusters is 2
-    2. Store the trend patterns in a normalised vector format
-        a. Should I do that in the section where I split them, so it just carries it over
-        b. Values between -1 and +1
-        
-        
-The patterns is normalised after being segmented
-Make a list for each one, th
-'''
